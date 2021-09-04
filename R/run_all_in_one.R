f7b75d743ae9cf424a86d6134a5ed8fa1b629861
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-09-04 17:05:41 -0500

    more item

---
+++ R/run_all_in_one.R
@@ -0,0 +1,19 @@
+## item_code = "U04a000S" ## cannot convert to long format (msm)
+item_code = "U19a000S"
+source("R/all_in_one.R")
+
+## item_code = "U01b000S"
+item_code = "U07x000S"
+source("R/all_in_one.R")
+
+item_code = "U02x000S"
+source("R/all_in_one.R")
+
+item_code = "U16x000S"
+source("R/all_in_one.R")
+
+item_code = "U11b000S"
+source("R/all_in_one.R")
+
+item_code = "U23x000S"
+source("R/all_in_one.R")
