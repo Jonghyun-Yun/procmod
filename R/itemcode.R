3f8498d98be6fb3f936752cb6b67620b8e276d8e
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-09-04 17:04:19 -0500

    more item + figure

--- R/itemcode.R
+++ R/itemcode.R
@@ -39,8 +39,6 @@ ignore_desc = c(
 "KEYPRESS")
 }
 
-## instruction is not clear. too many actions
-## respondents got confused (create or not create a folder to organize??)
 if (item_code == "U06a000S") {
 sub_str = rbind(
   c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
@@ -57,9 +55,151 @@ ignore_desc = c(
 "KEYPRESS")
 }
 
-## instruction is not clear. too many actions
-## respondents got confused (create or not create a folder to organize??)
+if (item_code == "U06b000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
 if (item_code == "U21x000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U04a000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U19a000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U19b000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U07x000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U16x000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U02x000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U11b000S") {
+sub_str = rbind(
+  c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
+  c("id=u03a_",  ""),
+  c("id=u04a_",  ""),
+  c("id=u06a_",  ""),
+  c("\\*\\$target=", ""),
+  c("\\*\\$value=", ""),
+  c("\\*\\$index=", ""),
+  c("\\*\\$href=", ""))
+ignore_desc = c(
+"START",
+"END",
+"KEYPRESS")
+}
+
+if (item_code == "U23x000S") {
 sub_str = rbind(
   c("(.*)\\*\\$target=u03a_(.*)","\\1\\2"),
   c("id=u03a_",  ""),
