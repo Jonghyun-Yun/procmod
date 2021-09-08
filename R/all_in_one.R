92a71315947fe86f95e356847476a1eb57f0a434
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-09-08 16:52:49 -0500

    Rscripts

--- R/all_in_one.R
+++ R/all_in_one.R
@@ -1,3 +1,7 @@
+ipath = "input"
+if (dir.exists(paths)) {
+  do.call(file.remove, list(list.files("input", full.names = TRUE)))
+  } else dir.create(ipath)
 source("R/itemcode.R")
 out_dir = paste0(booklet$NAME[booklet$CODEBOOK == item_code], "/")
 system(paste0("figlet ", item_code))
--- R/run_all_in_one.R
+++ R/run_all_in_one.R
@@ -1,8 +1,9 @@
 ## item_code = "U04a000S" ## cannot convert to long format (msm)
+## item_code = "U01b000S"
+
 item_code = "U19a000S"
 source("R/all_in_one.R")
 
-## item_code = "U01b000S"
 item_code = "U07x000S"
 source("R/all_in_one.R")
 
@@ -16,4 +17,7 @@ item_code = "U11b000S"
 source("R/all_in_one.R")
 
 item_code = "U23x000S"
+source("R/all_in_one.R")
+
+item_code = "U06a000S"
 source("R/all_in_one.R")
