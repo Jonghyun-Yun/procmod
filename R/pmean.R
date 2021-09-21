976520b0f5cdade8bf0e89e332ee92ae90e97d52
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-09-21 15:31:25 -0500

    pdf figures

---
+++ R/pmean.R
@@ -0,0 +1,16 @@
+param_path = paste0(out_dir, "param_mean.csv")
+if (file.exists(param_path)) {
+  dout = readr::read_csv(param_path)
+} else {
+  ss <- summary(mclist)
+  mm <- ss$statistics[, "Mean"]
+  ## rr <- c(grep("^kappa", names(mm)), grep("^tau", names(mm)))
+  ## dout <- data.frame(vname = names(mm[rr]), mean = mm[rr])
+  dout <- data.frame(vname = names(mm), mean = mm)
+  readr::write_csv(dout, param_path)
+}
+mtheta <- dout[grep("^theta", dout$vname), 2]
+mtau <- dout[grep("^tau", dout$vname), 2]
+mkappa <- dout[grep("^kappa", dout$vname), 2]
+
+item <- readr::read_csv(paste0(out_dir, "input/item.csv"), col_names = TRUE)
